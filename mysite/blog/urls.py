ccf32e3dd7472412f031c3522d840e96c8abf0e0
Kevin Nguyen <KNguyenSky@gmail.com>
2023-04-15 16:15:14 -0400

    Created urls for blog and mysite. As well as this we created templates and utilized views in blog app.

---
+++ mysite/blog/urls.py
@@ -0,0 +1,8 @@
+# Blog url paths
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.post_list, name="post_list"),
+]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    
+	# Render an html file, empty dict. so no variables accessed in html
+	# django file 
+    return render(request, "blog/post_list.html", {})
